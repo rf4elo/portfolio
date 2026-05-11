cb716eb06028538a359446b19f4ddbc86862a506
Rafael <rafaelrocha57852@gmail.com>
2026-05-11 16:57:48 -0300

    Re-update the Page Not Found

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -4,7 +4,7 @@
 
 export function PageNotFound() {
     return (
-        <div className="text-white mn-h-screen flex flex-col justify-center items-center" >
+        <div className="text-white h-screen flex flex-col justify-center items-center" >
             <h1 className="text-6xl font-bold text-center mt-20" >404 - Página Não Encontrada</h1>
             <p className="text-xl text-center mt-4" >Ops! A página que você está procurando não existe.</p>
         </div>
